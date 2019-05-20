c545fec58e9559ce426950c1433d574939c59fad
yurkiss <terranet23@gmail.com>
2019-05-21 01:45:56 +0300

    total recoding

--- Server/MainWindow.xaml.cs
+++ Server/MainWindow.xaml.cs
@@ -3,7 +3,10 @@ using Server.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,19 +25,68 @@ namespace Server
     /// </summary>
     public partial class MainWindow : Window
     {
-        private EFContext _context;
+        //private EFContext _context;
+        public string EPoint { get; set; }
+        public string NameImg { get; set; }
+        public static string RecMessage { get; set; }
         public MainWindow()
         {
             InitializeComponent();
-            _context = new EFContext();
+            //_context = new EFContext();
 
-            List<ImageModel> l = new List<ImageModel>(_context.Images.Select(i => new ImageModel()
+            Task SrvStart = new Task(ServerStart);
+            SrvStart.Start();
+
+            //List<ImageModel> l = new List<ImageModel>(_context.Images.Select(i => new ImageModel()
+            //{
+            //    Id = i.Id,
+            //    Name = i.Name,
+            //    Base64 = i.Base64
+            //}).ToList());
+            //dg.ItemsSource = l;
+
+            Thread.Sleep(1000);
+            this.Title = EPoint;
+        }
+
+        public void ServerStart()
+        {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ep = new IPEndPoint(ip, 1098);
+            EPoint = ep.ToString();
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+            s.Bind(ep);
+            s.Listen(10);
+            try
             {
-                Id = i.Id,
-                Name = i.Name,
-                Base64 = i.Base64
-            }).ToList());
-            dg.ItemsSource = l;
+                while (true)
+                {
+                    Socket ns = s.Accept();
+                    string data = null;
+                    byte[] bytes = new byte[1024];
+                    int bytesRec = ns.Receive(bytes);
+                    data += Encoding.UTF8.GetString(bytes, 0, bytesRec);
+                    NameImg = data;
+                    ns.Send(Encoding.UTF8.GetBytes($" sending {DateTime.Now}"));
+                    ns.Shutdown(SocketShutdown.Both);
+                    ns.Close();
+                }
+            }
+            catch (SocketException ex)
+            {
+                MessageBox.Show("Socket error: " + ex.Message);
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            WindowSend send = new WindowSend();
+            send.ShowDialog();
+
+            if (!string.IsNullOrEmpty(NameImg))
+            {
+                txtNameImg.Text = NameImg;
+            }
         }
     }
 }
---
+++ Server/WindowSend.xaml.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Win32;
+using Server.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Drawing;
+
+namespace Server
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowSend.xaml
+    /// </summary>
+    public partial class WindowSend : Window
+    {
+        private EFContext _context;
+        public string NameImg { get; set; }
+        public System.Drawing.Image AddImg { get; set; }
+        public WindowSend()
+        {
+            InitializeComponent();
+            _context = new EFContext();
+
+        }
+
+        private void BtnAddImg_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                if (ofd.ShowDialog() == true)
+                {
+                    NameImg = ofd.SafeFileName;
+                    AddImg = System.Drawing.Image.FromFile(ofd.FileName);
+                    imgAddImg.Source = new BitmapImage(new Uri(ofd.FileName));
+                    txtNameImg.Text = NameImg;
+                }
+            }
+            catch
+            {
+                throw new Exception("errors!");
+            }
+        }
+
+        private void BtnSendImg_Click(object sender, RoutedEventArgs e)
+        {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ep = new IPEndPoint(ip, 1098);
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+            try
+            {
+                s.Connect(ep);
+                if (s.Connected)
+                {
+                    s.Send(Encoding.UTF8.GetBytes(txtNameImg.Text));
+                    byte[] buffer = new byte[1024];
+                    int l;
+                    do
+                    {
+                        l = s.Receive(buffer);
+                    } while (l > 0);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                this.Close();
+            }
+            this.Close();
+        }
+    }
+}
